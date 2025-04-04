c255e4ba3cf218d75c81c9217d82653c78629555
이주형 <leewngud980411@gmail.com>
2025-04-05 00:43:09 +0900

    [feat] StatusBarText 색상을 흰색으로 강제

--- app/src/main/java/com/wngud/pomosound/MainActivity.kt
+++ app/src/main/java/com/wngud/pomosound/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.wngud.pomosound
 
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.wngud.pomosound.ui.theme.PomoSoundTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,10 +15,19 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        forceLightStatusBarText(window)
+
         setContent {
             PomoSoundTheme {
                 PomoSoundApp()
             }
         }
     }
+}
+
+fun forceLightStatusBarText(window: Window) {
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+    val controller = WindowInsetsControllerCompat(window, window.decorView)
+    controller.isAppearanceLightStatusBars = false
 }

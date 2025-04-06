88dfae36437b9f38cf2abc1652b84788a3727855
이주형 <leewngud980411@gmail.com>
2025-04-06 21:04:28 +0900

    [feat] CustomTopAppBar 컴포넌트

--- app/src/main/java/com/wngud/pomosound/ui/components/CustomTopAppBar.kt
+++ app/src/main/java/com/wngud/pomosound/ui/components/CustomTopAppBar.kt
@@ -3,10 +3,13 @@ package com.wngud.pomosound.ui.components
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -19,14 +22,17 @@ fun CustomTopAppBar(
 ) {
     TopAppBar(
         title = {
-            Text(text = title)
+            Text(
+                text = title,
+                fontWeight = FontWeight.Bold,
+            )
         },
         navigationIcon = navigationIcon?.let { icon ->
             {
                 IconButton(onClick = onNavigationClick) {
                     Icon(
                         imageVector = icon,
-                        contentDescription = "Navigation icon"
+                        contentDescription = "Navigation icon",
                     )
                 }
             }
@@ -36,10 +42,13 @@ fun CustomTopAppBar(
                 IconButton(onClick = onActionClick) {
                     Icon(
                         imageVector = icon,
-                        contentDescription = "Action icon"
+                        contentDescription = "Action icon",
                     )
                 }
             }
         } ?: {}
+        , colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.background
+        )
     )
 }
